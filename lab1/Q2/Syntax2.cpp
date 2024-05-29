7b2ee9baf0fd1bc8e566542c02bf2faeead3da15
Josiah <157635371+JOSIAHTHEPROGRAMMER@users.noreply.github.com>
2024-05-29 16:17:09 -0400

    Update Syntax2.cpp

--- lab1/Q2/Syntax2.cpp
+++ lab1/Q2/Syntax2.cpp
@@ -1,10 +1,34 @@
+//Question 2: Correct errors and show output:
+
+//Error 1
 #include iostream
 
+//Error 2
 using name space stdio;
 
 int main () {
+  //Error 3
 	cout << "I'm going to ace Programming 1!"endl;
+  
+  //Error 4
     cout<<endl
+      
     cout>>"This is lots of fun";
+  
+  //Error 5
 	return ;
 }
+
+// Corrected:
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   cout << "I'm going to ace Programming 1!"<<endl;
+//   cout<<endl;
+//   cout<<"This is lots of fun";
+//   return 0;
+//}
+//OUTPUT: 
+//I'm going to ace Programming 1!
+//
+//This is lots of fun
