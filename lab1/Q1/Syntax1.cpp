87f9a67d9071bae281f6b35781365a8fd7b523c4
Josiah <157635371+JOSIAHTHEPROGRAMMER@users.noreply.github.com>
2024-05-29 15:42:59 -0400

    Add files via upload

---
+++ lab1/Q1/Syntax1.cpp
@@ -0,0 +1,24 @@
+//Question: Find all Errors in Code
+
+
+// Error 1 
+#inlcude <iostream>
+
+using namespace std;
+
+//Error 2
+integer main () {
+  //Error 3
+	cout >> "Can you find my errors?',
+    
+	return 0;
+}
+
+// Fixed:
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   cout << "Can you find my errors?";
+//   return 0;
+//}
+//OUTPUT: Can you find my errors?
